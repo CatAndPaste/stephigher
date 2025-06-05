915c8a3ffd3c2090587862c8d327300c6ad2d9bb
alskarru <alskarru@gmail.com>
2025-06-05 16:00:50 +0300

    post views design updated

--- app/news/urls.py
+++ app/news/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('', NewsListView.as_view(), name='news_list'),
     path('<slug:slug>/', NewsDetailView.as_view(), name='news_detail'),
-    path('<slug:slug>/comments/', news_load_more_comments, name='news_load_more_comments'),
+    path('<slug:slug>/comments/', news_load_more_comments, name='news_load_comments'),
     path('comment/<int:comment_id>/delete/', news_delete_comment, name='news_delete_comment'),
 ]
